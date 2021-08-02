75e7c5a14fde8e6da7da1a1c2f364ab7c91ccc3f
Liudmyla Yevchuck <lol22lil22@gmail.com>
2021-08-02 16:58:15 +0300

    Updates

---
+++ precache-manifest.616beb87b83798c24de7cc659460b558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a387d29e21d5918a9cffbc8df2710304",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fa6a1a722fa2c4fd76a",
+    "url": "./static/css/main.166bc5de.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa6a1a722fa2c4fd76a",
+    "url": "./static/js/main.24bd2734.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
